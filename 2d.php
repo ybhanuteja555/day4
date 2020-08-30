4a660ccd6c9ed6382dff23b46b08310577b63bbf
ybhanuteja555 <ybhanuteja555@gmail.com>
2020-08-30 20:41:10 +0530

    php file

---
+++ 2d.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>FORM</title>
+</head>
+<br>
+<body>
+
+<h2>Wel-come to Student's Details page</h2>
+<br><br><br>
+
+
+<?php
+	echo $_GET["FNAME"];
+	echo $_GET["Number"];
+	echo $_GET["COC"];
+	echo $_GET["SI"];
+?>
+
+
+</body>
+</html>
